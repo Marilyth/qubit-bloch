18c5b67e41b53ee8853d3cdb7dc06e15d4ea50a2
May Bär <m.baer@iasonconsulting.com>
2025-01-27 14:33:03 +0100

    Use vectors

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ class BlochSphere:
         self.qubit_label = self.ax_sphere.text(0, 0, 0, '|ψ⟩', color='black', fontsize=12, ha='center', va='center')
 
         # UI elements, Position: [left, bottom, width, height]
-        self.slider_theta = Slider(plt.axes([0.1, 0.10, 0.35, 0.03]), 'θ (degrees)', 0, 360, valinit=0)
-        self.slider_phi = Slider(plt.axes([0.1, 0.15, 0.35, 0.03]), 'ϕ (degrees)', 0, 360, valinit=0)
+        self.slider_theta = Slider(plt.axes([0.1, 0.10, 0.35, 0.03]), 'θ (degrees)', 0, 180, valinit=0)
+        self.slider_phi = Slider(plt.axes([0.1, 0.15, 0.35, 0.03]), 'ϕ (degrees)', -180, 180, valinit=0)
         self.x_gate_button = Button(plt.axes([0.1, 0.20, 0.05, 0.03]), 'X')
         self.y_gate_button = Button(plt.axes([0.15, 0.20, 0.05, 0.03]), 'Y')
         self.z_gate_button = Button(plt.axes([0.2, 0.20, 0.05, 0.03]), 'Z')
@@ -73,50 +73,45 @@ class BlochSphere:
         self.t_gate_button.on_clicked(self.t_gate_animation)
         self.measure_button.on_clicked(self.measure)
 
+        self.zero_value = 1 + 0j
+        self.one_value = 0 + 0j
+
         self.update(0)
 
-    def wavefunction(self, theta, phi):
-        """Returns the |0⟩ and |1⟩ amplitudes of a qubit state given theta and phi angles in degrees."""
-        theta_rad = np.deg2rad(theta)
-        phi_rad = np.deg2rad(phi)
-        wf_0 = np.cos(theta_rad / 2)
-        wf_1 = np.exp(1j * phi_rad) * np.sin(theta_rad / 2)
+    def get_rotations(self):
+        """Returns the theta and phi rotations of the qubit state on the Bloch Sphere."""
+        zero_value = self.zero_value  # Amplitude for |0⟩
+        one_value = self.one_value    # Amplitude for |1⟩
 
-        if self.y_flip_value != 0:
-            # Interpolate toward the Y-gate result
-            relative_wf0 = (1 - self.y_flip_value) * wf_0 + (wf_1 * -1j) * self.y_flip_value
-            relative_wf1 = (1 - self.y_flip_value) * wf_1 + (wf_0 * 1j) * self.y_flip_value
+        # Magnitudes of the complex numbers
+        abs_zero = np.abs(zero_value)
+        abs_one = np.abs(one_value)
 
-            # Normalize to ensure unit length
-            norm = np.sqrt(np.abs(relative_wf0)**2 + np.abs(relative_wf1)**2)
-            wf_0 = relative_wf0 / norm
-            wf_1 = relative_wf1 / norm
+        # Calculate theta (polar angle) based on the magnitude of the |0⟩ state
+        theta = 2 * np.arccos(abs_zero)  # theta is 2 * arccos(|zero_value|)
 
-        return wf_0, wf_1
+        # Calculate the phase difference between |0⟩ and |1⟩ states
+        phi = np.angle(one_value) - np.angle(zero_value)
+
+        return np.rad2deg(theta), np.rad2deg(phi)
 
     def get_bloch_coordinates(self):
         """Returns the X, Y, Z coordinates of the qubit state on the Bloch Sphere."""
-        wf_0, wf_1 = self.wavefunction(self.slider_theta.val, self.slider_phi.val)
-
-        # Magnitude of the amplitude
-        abs_wf_0 = np.abs(wf_0)
-
-        # Calculate theta
-        theta = 2 * np.arccos(abs_wf_0)
-
-        # Calculate the phases of wf_0 and wf_1
-        phi_0 = np.angle(wf_0)  # phase of wf_0
-        phi_1 = np.angle(wf_1)  # phase of wf_1
-
-        # Calculate the relative phase phi
-        phi = phi_1 - phi_0
+        theta, phi = self.slider_theta.val, self.slider_phi.val
 
         # Compute the Bloch sphere coordinates
-        x = np.sin(theta) * np.cos(phi)
-        y = np.sin(theta) * np.sin(phi)
-        z = np.cos(theta)
+        x = np.sin(np.deg2rad(theta)) * np.cos(np.deg2rad(phi))
+        y = np.sin(np.deg2rad(theta)) * np.sin(np.deg2rad(phi))
+        z = np.cos(np.deg2rad(theta))
 
         return x, y, z
+
+    def wavefunction(self):
+        """Returns the wavefunction amplitudes for the given theta and phi angles."""
+        zero_value = np.cos(np.deg2rad(self.slider_theta.val / 2))
+        one_value = np.sin(np.deg2rad(self.slider_theta.val / 2)) * np.exp(1j * np.deg2rad(self.slider_phi.val))
+
+        return zero_value, one_value
 
     def draw_qubit(self):
         """Draws the qubit state on the Bloch Sphere."""
@@ -128,80 +123,95 @@ class BlochSphere:
         self.qubit_label.remove()
         self.qubit_label = self.ax_sphere.text(x * 1.1, y * 1.1, z * 1.1, '|ψ⟩', color='black', fontsize=12, ha='center', va='center')
 
-        quantum_state = self.wavefunction(self.slider_theta.val, self.slider_phi.val)
-        self.ax_sphere.set_title(f"Bloch Sphere\n{np.round(quantum_state[0], 2)}∣0⟩\n{np.round(quantum_state[1], 2)}∣1⟩")
+        zero_value, one_value = self.wavefunction()
+        self.ax_sphere.set_title(f"Bloch Sphere\n{np.round(zero_value, 2)}∣0⟩\n{np.round(one_value, 2)}∣1⟩")
 
     def update(self, val):
         """Update the plot with the new qubit state."""
         self.draw_qubit()
 
         # Update wavefunction probabilities
-        wf_0, wf_1 = self.wavefunction(self.slider_theta.val, self.slider_phi.val)
+        wf_0, wf_1 = self.wavefunction()
         self.bar[0].set_height(np.abs(wf_0)**2)
         self.bar[1].set_height(np.abs(wf_1)**2)
 
         # Redraw only the modified elements
         self.fig.canvas.draw_idle()
 
-    def x_gate_animation(self, val):
-        """Animate the X-gate operation."""
-        initial_theta = self.slider_theta.val
+    def animate_rotations(self):
+        theta, phi = self.get_rotations()
+        current_theta = self.slider_theta.val
+        current_phi = self.slider_phi.val
 
-        for theta in np.linspace(0, 180, 50):
-            self.slider_theta.set_val((initial_theta + theta) % 360)
+        for new_theta, new_phi in zip(np.linspace(current_theta, theta, 20), np.linspace(current_phi, phi, 20)):
+            self.slider_theta.set_val(new_theta)
+            self.slider_phi.set_val(new_phi)
             self.update(0)
             plt.pause(0.01)
+
+    def x_gate_animation(self, val):
+        """Animate the X-gate operation. Which multiplies the qubit state by the Pauli-X matrix."""
+        pauli_x = np.array([[0, 1], [1, 0]])
+        components = np.array([self.zero_value, self.one_value])
+        new_components = np.dot(pauli_x, components)
+        self.zero_value = new_components[0]
+        self.one_value = new_components[1]
+
+        self.animate_rotations()
 
     def y_gate_animation(self, val):
         """Animate the Y-gate operation."""
-        initial_y_flip = -self.y_flip_value
+        pauli_y = np.array([[0, -1j], [1j, 0]])
+        components = np.array([self.zero_value, self.one_value])
+        new_components = np.dot(pauli_y, components)
+        self.zero_value = new_components[0]
+        self.one_value = new_components[1]
 
-        for y_flip in np.linspace(abs(initial_y_flip), initial_y_flip + 1, 50):
-            self.y_flip_value = y_flip
-            self.update(0)
-            plt.pause(0.01)
-
-        self.update(0)
+        self.animate_rotations()
 
     def z_gate_animation(self, val):
         """Animate the Z-gate operation."""
-        initial_phi = self.slider_phi.val
+        pauli_z = np.array([[1, 0], [0, -1]])
+        components = np.array([self.zero_value, self.one_value])
+        new_components = np.dot(pauli_z, components)
+        self.zero_value = new_components[0]
+        self.one_value = new_components[1]
 
-        for phi in np.linspace(0, 180, 50):
-            self.slider_phi.set_val((initial_phi + phi) % 360)
-            self.update(0)
-            plt.pause(0.01)
+        self.animate_rotations()
 
     def s_gate_animation(self, val):
         """Animate the S-gate operation."""
-        initial_phi = self.slider_phi.val
+        phase = np.array([[1, 0], [0, 1j]])
+        components = np.array([self.zero_value, self.one_value])
+        new_components = np.dot(phase, components)
+        self.zero_value = new_components[0]
+        self.one_value = new_components[1]
 
-        for phi in np.linspace(0, 90, 25):
-            self.slider_phi.set_val((initial_phi + phi) % 360)
-            self.update(0)
-            plt.pause(0.01)
+        self.animate_rotations()
 
     def t_gate_animation(self, val):
         """Animate the T-gate operation."""
-        initial_phi = self.slider_phi.val
+        t = np.array([[1, 0], [0, np.exp(1j * np.pi / 4)]])
+        components = np.array([self.zero_value, self.one_value])
+        new_components = np.dot(t, components)
+        self.zero_value = new_components[0]
+        self.one_value = new_components[1]
 
-        for phi in np.linspace(0, 45, 12):
-            self.slider_phi.set_val((initial_phi + phi) % 360)
-            self.update(0)
-            plt.pause(0.01)
+        self.animate_rotations()
     
     def h_gate_animation(self, val):
         """Animate the H-gate operation."""
-        initial_theta = self.slider_theta.val
+        h = np.array([[1, 1], [1, -1]]) / np.sqrt(2)
+        components = np.array([self.zero_value, self.one_value])
+        new_components = np.dot(h, components)
+        self.zero_value = new_components[0]
+        self.one_value = new_components[1]
 
-        for theta in np.linspace(0, 90, 25):
-            self.slider_theta.set_val((initial_theta + theta) % 360)
-            self.update(0)
-            plt.pause(0.01)
+        self.animate_rotations()
 
     def measure(self, val):
         """Simulate a measurement on the qubit state."""
-        wf_0, wf_1 = self.wavefunction(self.slider_theta.val, self.slider_phi.val)
+        wf_0, wf_1 = self.wavefunction()
         prob_0 = np.abs(wf_0) ** 2
         prob_1 = np.abs(wf_1) ** 2
 
@@ -213,9 +223,14 @@ class BlochSphere:
         if state == 0:
             self.slider_theta.set_val(0)
             self.slider_phi.set_val(0)
+            self.one_value = 0 + 0j
+            self.zero_value = 1 + 0
         else:
             self.slider_theta.set_val(180)
             self.slider_phi.set_val(0)
+            self.one_value = 1 + 0j
+            self.zero_value = 0 + 0j
+
 
 # Initialize plot
 sphere = BlochSphere()
